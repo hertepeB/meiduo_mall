98402fbda8fe40b42be250424782534f391462fa
hertepeB <daozhi19909@126.com>
2021-05-28 10:02:55 +0800

    立项准备之创建配置文件

--- meiduo_mall/manage.py
+++ meiduo_mall/manage.py
@@ -3,7 +3,8 @@ import os
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "meiduo_mall.settings")
+    # 使用meiduo_mall.settings.dev配置文件启动开发环境的项目
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "meiduo_mall.settings.dev")
     try:
         from django.core.management import execute_from_command_line
     except ImportError:
